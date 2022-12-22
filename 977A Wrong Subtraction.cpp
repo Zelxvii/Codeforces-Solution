35ed0483af446e125722abb4dcfe164c240a5033
Zelxvii <92300919+Zelxvii@users.noreply.github.com>
2022-12-22 18:56:42 +0700

    Create 977A Wrong Subtraction.cpp

---
+++ 977A Wrong Subtraction.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n, k;
+	cin >> n >> k;
+	for(int i = 1; i <= k; i++){
+		if(n % 10 !=0){
+			n--;
+		}
+		else{
+			n = n / 10;
+		}
+	}
+	cout << n;
+}
